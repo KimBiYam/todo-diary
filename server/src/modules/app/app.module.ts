d40346e2528ed6ee8bb05baec446764c36d2d176
ChangHyunKim <hot9998@naver.com>
2021-06-15 20:29:50 +0900

    Add auth module

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/user';
+import { UserModule } from '../user';
 import { APP_FILTER } from '@nestjs/core';
 import { configModuleOption, typeormConfig } from 'src/config/';
 import { ConfigModule } from '@nestjs/config';
 import { CustomExceptionFilter } from 'src/filters/custom-exception.filter';
+import { AuthModule } from '../auth';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig),
     ConfigModule.forRoot(configModuleOption),
     UserModule,
+    AuthModule,
   ],
   providers: [{ provide: APP_FILTER, useClass: CustomExceptionFilter }],
 })
