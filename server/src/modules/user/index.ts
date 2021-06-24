a7cf8ddaf42d62e589d713a26e923fdc8846f38a
ChangHyunKim <hot9998@naver.com>
2021-06-24 16:41:08 +0900

    Remove user controller

--- server/src/modules/user/index.ts
+++ server/src/modules/user/index.ts
@@ -1,3 +1,2 @@
 export * from './user.module';
 export * from './user.service';
-export * from './user.controller';
--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@src/entities';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
