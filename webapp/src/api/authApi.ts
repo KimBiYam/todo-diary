98cd2b278591412d99c163f00e54358f34182a05
ChangHyunKim <hot9998@naver.com>
2021-07-04 00:05:59 +0900

    Change authApi, tokenStorage structure

--- webapp/src/api/authApi.ts
+++ webapp/src/api/authApi.ts
@@ -1,62 +1,67 @@
 import { User } from '../types/user.types';
 import apiClient from './apiClient';
 
+const getUserProfile = async () => {
+  try {
+    const response = await apiClient.get('/api/auth/user/profile');
+    const { email, displayName, photoUrl } = response.data;
+
+    const user: User = { email, displayName, photoUrl };
+    return user;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+const checkGoogleAccount = async (googleToken: string) => {
+  try {
+    const response = await apiClient.get('/api/auth/google/check', {
+      params: { googleToken },
+    });
+
+    return response.data.exists;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+const signInGoogleAccount = async (googleToken: string) => {
+  try {
+    const response = await apiClient.post('/api/auth/google/sign-in', {
+      googleToken,
+    });
+
+    const data = response.data;
+    const { accessToken, email, displayName, photoUrl } = data;
+
+    const user: User = { email, displayName, photoUrl };
+    return { user, accessToken };
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+const signUpGoogleAccount = async (googleToken: string) => {
+  try {
+    const response = await apiClient.post('/api/auth/google/sign-up', {
+      googleToken,
+    });
+
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
 const authApi = {
-  async getUserProfile() {
-    try {
-      const response = await apiClient.get('/api/auth/user/profile');
-      const { email, displayName, photoUrl } = response.data;
-
-      const user: User = { email, displayName, photoUrl };
-      return user;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  },
-
-  async checkGoogleAccount(googleToken: string) {
-    try {
-      const response = await apiClient.get('/api/auth/google/check', {
-        params: { googleToken },
-      });
-
-      return response.data.exists;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  },
-
-  async signInGoogleAccount(googleToken: string) {
-    try {
-      const response = await apiClient.post('/api/auth/google/sign-in', {
-        googleToken,
-      });
-
-      const data = response.data;
-      const { accessToken, email, displayName, photoUrl } = data;
-
-      const user: User = { email, displayName, photoUrl };
-      return { user, accessToken };
-    } catch (e) {
-      console.log(e);
-      throw e;
-    }
-  },
-
-  async signUpGoogleAccount(googleToken: string) {
-    try {
-      const response = await apiClient.post('/api/auth/google/sign-up', {
-        googleToken,
-      });
-
-      return response.data;
-    } catch (e) {
-      console.log(e);
-      throw e;
-    }
-  },
+  getUserProfile,
+  checkGoogleAccount,
+  signInGoogleAccount,
+  signUpGoogleAccount,
 };
 
 export default authApi;
--- webapp/src/storage/tokenStorage.ts
+++ webapp/src/storage/tokenStorage.ts
@@ -1,27 +1,27 @@
 const ACCESS_TOKEN = 'ACCESS_TOKEN';
 
-const tokenStorage = {
-  getToken() {
-    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+const getToken = () => {
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
 
-    if (accessToken === null) {
-      return null;
-    }
+  if (accessToken === null) {
+    return null;
+  }
 
-    return accessToken;
-  },
-
-  setToken(accessToken: string) {
-    localStorage.setItem(ACCESS_TOKEN, accessToken);
-  },
-
-  clearToken() {
-    localStorage.removeItem(ACCESS_TOKEN);
-  },
-
-  isTokenExists() {
-    return !!this.getToken();
-  },
+  return accessToken;
 };
+
+const setToken = (accessToken: string) => {
+  localStorage.setItem(ACCESS_TOKEN, accessToken);
+};
+
+const clearToken = () => {
+  localStorage.removeItem(ACCESS_TOKEN);
+};
+
+const isTokenExists = () => {
+  return !!getToken();
+};
+
+const tokenStorage = { getToken, setToken, clearToken, isTokenExists };
 
 export default tokenStorage;
