96e990b50d634fa9e995394c5797ce8880397100
ChangHyunKim <hot9998@naver.com>
2021-07-10 18:20:08 +0900

    Change useUser hook to use useTypedSelector

--- webapp/src/hooks/useUser.ts
+++ webapp/src/hooks/useUser.ts
@@ -1,6 +1,6 @@
 import { useQueryClient } from 'react-query';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../reducers/store';
+import { useDispatch } from 'react-redux';
+import { useTypedSelector } from '../reducers/rootReducer';
 import { login, logout } from '../reducers/user';
 import tokenStorage from '../storage/tokenStorage';
 import { User } from '../types/auth.types';
@@ -8,10 +8,9 @@ import { User } from '../types/auth.types';
 const useUser = () => {
   const queryClient = useQueryClient();
   const dispatch = useDispatch();
-  const user = useSelector((state: RootState) => state.user.user);
-  const isLoggedIn = useSelector(
-    (state: RootState) =>
-      tokenStorage.isTokenExists() && state.user.user !== undefined,
+  const user = useTypedSelector((state) => state.user.user);
+  const isLoggedIn = useTypedSelector(
+    (state) => tokenStorage.isTokenExists() && state.user.user !== undefined,
   );
 
   const userLogIn = (user: User) => {
