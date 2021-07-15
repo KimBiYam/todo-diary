af7416075043295302553ea2d3bc91d9777e6841
ChangHyunKim <hot9998@naver.com>
2021-07-15 19:01:40 +0900

    Add dialog animation

--- webapp/src/components/base/AppDialog.tsx
+++ webapp/src/components/base/AppDialog.tsx
@@ -1,50 +1,71 @@
 import { css } from '@emotion/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { setTimeout } from 'timers';
 import useDialog from '../../hooks/useDialog';
+import { fadeIn, fadeOut } from '../../styles/transitions';
 import { Z_INDEXES } from '../../styles/zIndexes';
 import Dialog from '../common/Dialog';
 
 export type AppDialogProps = {};
 
 const AppDialog = () => {
+  const [visible, setVisible] = useState(false);
   const {
     closeDialog,
     dialog: { isOpen, text },
   } = useDialog();
 
   useEffect(() => {
-    const openTime = 1 * 1000;
-    let timeoutId: NodeJS.Timeout;
+    const transitionTime = 1 * 1000;
+    let openTimeoutId: NodeJS.Timeout;
 
     if (isOpen) {
-      timeoutId = setTimeout(() => closeDialog(), openTime);
+      setVisible(true);
+      openTimeoutId = setTimeout(() => setVisible(false), transitionTime);
     }
 
     return () => {
-      if (timeoutId !== undefined) {
-        clearTimeout(timeoutId);
+      if (openTimeoutId !== undefined) {
+        clearTimeout(openTimeoutId);
       }
     };
   }, [isOpen]);
+
+  useEffect(() => {
+    const openTime = 1 * 1000;
+    let visibleTimeoutId: NodeJS.Timeout;
+
+    if (isOpen && !visible) {
+      visibleTimeoutId = setTimeout(() => closeDialog(), openTime);
+    }
+
+    return () => {
+      if (visibleTimeoutId !== undefined) {
+        clearTimeout(visibleTimeoutId);
+      }
+    };
+  }, [visible]);
 
   if (!isOpen || text === undefined) {
     return null;
   }
 
   return (
-    <div css={dialogWrapper}>
+    <div css={dialogWrapper(visible)}>
       <Dialog text={text} />
     </div>
   );
 };
 
-const dialogWrapper = css`
+const dialogWrapper = (visible: boolean) => css`
+  visibility: ${visible ? 'visible' : 'hidden'};
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: ${Z_INDEXES.dialog};
+  transition: visibility 1s ease-in-out;
+  animation: ${visible ? fadeIn : fadeOut} 1s ease-in-out;
 `;
 
 export default AppDialog;
--- webapp/src/styles/transitions.ts
+++ webapp/src/styles/transitions.ts
@@ -9,3 +9,23 @@ export const shining = keyframes`
         opacity: 1;
     }
 `;
+
+export const fadeIn = keyframes`
+    0% {
+        opacity: 0;
+    }
+
+    100% {
+        opacity: 1;
+    }
+`;
+
+export const fadeOut = keyframes`
+    0% {
+        opacity: 1;
+    }
+
+    100% {
+        opacity: 0;
+    }
+`;
