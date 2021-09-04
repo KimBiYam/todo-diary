8abe532375b84d2d6a59fff7f599504c6548bb51
ChangHyunKim <hot9998@naver.com>
2021-09-05 03:49:12 +0900

    Change backend server url

--- webapp/src/constants/constants.ts
+++ webapp/src/constants/constants.ts
@@ -2,7 +2,7 @@ const IS_DEVELOPMENT_MODE = process.env.NODE_ENV === 'development';
 
 export const BACKEND_SERVER_URL = IS_DEVELOPMENT_MODE
   ? 'http://localhost:5000'
-  : '';
+  : 'https://todo-diary-backend.herokuapp.com';
 
 export const CLIENT_SERVER_URL = IS_DEVELOPMENT_MODE
   ? 'http://localhost:3000'
