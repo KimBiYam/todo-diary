14fd76281abdbd31f261ad5e752193a29a568f5d
ChangHyunKim <hot9998@naver.com>
2021-09-04 18:42:59 +0900

    Add loading spinner into github signin page

--- webapp/src/pages/GithubSignInPage.tsx
+++ webapp/src/pages/GithubSignInPage.tsx
@@ -1,3 +1,4 @@
+import { css, keyframes } from '@emotion/react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import authApi from '../api/authApi';
@@ -37,7 +38,35 @@ const GithubSignInPage = () => {
     }
   };
 
-  return <></>;
+  return (
+    <div css={box}>
+      <div css={spinner} />
+    </div>
+  );
 };
+
+const box = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const spinner = css`
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  animation: ${spin} 2s linear infinite;
+
+  ${spin}
+`;
 
 export default GithubSignInPage;
