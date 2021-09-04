998a38c08e1255b4063754fa5b21e9aa8be18a64
ChangHyunKim <hot9998@naver.com>
2021-09-05 03:52:43 +0900

    Fix port environment for heroku

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ import LoggingInterceptor from './interceptors/logging.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('SERVER_PORT') || 5000;
+  const port = configService.get('PORT') || 5000;
 
   if (configService.get('NODE_ENV') === 'DEVELOPMENT') {
     setupSwagger(app);
