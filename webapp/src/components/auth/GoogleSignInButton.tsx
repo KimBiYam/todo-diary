bfa09a8f67986f8dec9783828d3422add58d4bcd
ChangHyunKim <hot9998@naver.com>
2021-09-07 23:59:31 +0900

    Fix google sign in button props, add loading when google sign in

--- webapp/src/components/auth/GoogleSignInButton.tsx
+++ webapp/src/components/auth/GoogleSignInButton.tsx
@@ -2,65 +2,32 @@ import GoogleLogin, {
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
 } from 'react-google-login';
-import authApi from '../../api/authApi';
-import useDialogAction from '../../hooks/useDialogAction';
-import useUserAction from '../../hooks/useUserAction';
-import tokenStorage from '../../storage/tokenStorage';
 import SignInButton from './SignInButton';
 
-export type GoogleSignInButtonProps = {};
-
-const GoogleSignInButton = () => {
-  const { userLogIn, userLogOut } = useUserAction();
-  const { openDialog } = useDialogAction();
-
-  const handleSuccess = async (
+export type GoogleSignInButtonProps = {
+  onSuccess: (
     response: GoogleLoginResponse | GoogleLoginResponseOffline,
-  ) => {
-    try {
-      const { accessToken: googleToken } = response as GoogleLoginResponse;
-
-      const isExistsGoogleAccount = await authApi.checkGoogleAccount(
-        googleToken,
-      );
-
-      if (!isExistsGoogleAccount) {
-        await authApi.signUpGoogleAccount(googleToken);
-      }
-
-      await signIn(googleToken);
-    } catch (e) {
-      openDialog(String(e));
-      userLogOut();
-    }
-  };
-
-  const signIn = async (googleToken: string) => {
-    const signInResponse = await authApi.signInGoogleAccount(googleToken);
-
-    const { accessToken, user } = signInResponse;
-
-    tokenStorage.setToken(accessToken);
-    userLogIn(user);
-  };
-
-  const handleFailure = () => openDialog('서버 에러입니다');
-
-  return (
-    <GoogleLogin
-      clientId={String(process.env.REACT_APP_GOOGLE_CLIENT_ID)}
-      render={({ onClick, disabled }) => (
-        <SignInButton
-          label="Google 계정으로 로그인"
-          icon="googleIcon"
-          onClick={onClick}
-          disabled={disabled}
-        />
-      )}
-      onSuccess={handleSuccess}
-      onFailure={handleFailure}
-    />
-  );
+  ) => void;
+  onFailure: () => void;
 };
+
+const GoogleSignInButton = ({
+  onSuccess,
+  onFailure,
+}: GoogleSignInButtonProps) => (
+  <GoogleLogin
+    clientId={String(process.env.REACT_APP_GOOGLE_CLIENT_ID)}
+    render={({ onClick, disabled }) => (
+      <SignInButton
+        label="Google 계정으로 로그인"
+        icon="googleIcon"
+        onClick={onClick}
+        disabled={disabled}
+      />
+    )}
+    onSuccess={onSuccess}
+    onFailure={onFailure}
+  />
+);
 
 export default GoogleSignInButton;
