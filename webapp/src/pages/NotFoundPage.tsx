567bd130dd27b6ea4771dc9f47eaddb521b967ae
ChangHyunKim <hot9998@naver.com>
2021-09-09 23:54:26 +0900

    Fix not found page text size

--- webapp/src/pages/NotFoundPage.tsx
+++ webapp/src/pages/NotFoundPage.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import { Helmet } from 'react-helmet-async';
 import { useHistory } from 'react-router';
 import MainButton from '../components/common/MainButton';
+import { BREAK_POINTS } from '../styles/breakPoints';
 
 export type NotFoundPageProps = {};
 
@@ -34,14 +35,31 @@ const box = css`
 `;
 
 const errorTextWrapper = css`
-  font-size: 22rem;
+  font-size: 12rem;
+  font-weight: 500;
   user-select: none;
   font-smooth: antialiased;
+
+  ${BREAK_POINTS.medium} {
+    font-size: 16rem;
+  }
+
+  ${BREAK_POINTS.large} {
+    font-size: 22rem;
+  }
 `;
 
 const notFoundMessageWrapper = css`
-  font-size: 4rem;
+  font-size: 2.4rem;
   margin-bottom: 2.4rem;
+
+  ${BREAK_POINTS.medium} {
+    font-size: 3.2rem;
+  }
+
+  ${BREAK_POINTS.large} {
+    font-size: 4.8rem;
+  }
 `;
 
 export default NotFoundPage;
