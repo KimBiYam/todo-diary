c066b3a588590a1c409ea5bf0db90653a7c95917
ChangHyunKim <hot9998@naver.com>
2021-09-09 23:50:58 +0900

    Add styles into not found page

--- webapp/src/pages/NotFoundPage.tsx
+++ webapp/src/pages/NotFoundPage.tsx
@@ -1,16 +1,47 @@
+import { css } from '@emotion/react';
 import { Helmet } from 'react-helmet-async';
+import { useHistory } from 'react-router';
+import MainButton from '../components/common/MainButton';
 
 export type NotFoundPageProps = {};
 
 const NotFoundPage = () => {
+  const history = useHistory();
+
+  const handleClickHome = () => history.push('/');
+
   return (
     <>
       <Helmet>
         <title>Todo Diary | Not Found</title>
       </Helmet>
-      <div>NotFound!</div>
+      <div css={box}>
+        <div css={errorTextWrapper}>404</div>
+        <div css={notFoundMessageWrapper}>페이지를 찾을 수 없어요! :(</div>
+        <MainButton label="홈으로" color="tertiary" onClick={handleClickHome} />
+      </div>
     </>
   );
 };
+
+const box = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const errorTextWrapper = css`
+  font-size: 22rem;
+  user-select: none;
+  font-smooth: antialiased;
+`;
+
+const notFoundMessageWrapper = css`
+  font-size: 4rem;
+  margin-bottom: 2.4rem;
+`;
 
 export default NotFoundPage;
