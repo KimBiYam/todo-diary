27298dc6bebcec4f1d94cd8862076b710a97f389
ChangHyunKim <hot9998@naver.com>
2021-09-15 21:44:10 +0900

    Add logo to sign in page

--- webapp/src/assets/icons/index.tsx
+++ webapp/src/assets/icons/index.tsx
@@ -3,3 +3,4 @@ export { ReactComponent as write } from './write.svg';
 export { ReactComponent as chart } from './chart.svg';
 export { ReactComponent as githubIcon } from './github_icon.svg';
 export { ReactComponent as googleIcon } from './google_icon.svg';
+export { ReactComponent as logo } from './logo.svg';
