1db85ab7aa570b008806470d247a60e38ce63fe1
ChangHyunKim <hot9998@naver.com>
2021-09-23 15:47:10 +0900

    Fix date picker styles

--- webapp/src/components/common/DatePicker.tsx
+++ webapp/src/components/common/DatePicker.tsx
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import Picker, { registerLocale } from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.css';
 import { COLORS } from '../../constants';
 import Icon from './Icon';
 
