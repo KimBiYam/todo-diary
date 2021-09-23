0d0d49eeff5ce576f8d6b35b26f0fdea255edfdb
ChangHyunKim <hot9998@naver.com>
2021-09-23 15:26:34 +0900

    Change date picker icon

--- webapp/src/assets/icons/index.tsx
+++ webapp/src/assets/icons/index.tsx
@@ -4,3 +4,4 @@ export { ReactComponent as chart } from './chart.svg';
 export { ReactComponent as githubIcon } from './github_icon.svg';
 export { ReactComponent as googleIcon } from './google_icon.svg';
 export { ReactComponent as logo } from './logo.svg';
+export { ReactComponent as calendar } from './calendar.svg';
