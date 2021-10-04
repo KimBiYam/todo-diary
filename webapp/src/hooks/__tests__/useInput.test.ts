c81b32a54a77058b48df243922a7df23d31718db
ChangHyunKim <hot9998@naver.com>
2021-10-04 23:50:08 +0900

    Add use input hook's test

---
+++ webapp/src/hooks/__tests__/useInput.test.ts
@@ -0,0 +1,28 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { ChangeEvent } from 'react';
+import useInput from '../useInput';
+
+describe('useInput hook', () => {
+  it('should change value when use onChange function', async () => {
+    // given
+    const { result } = renderHook(() => useInput());
+    const input = 'input';
+
+    // when
+    act(() => {
+      const event = {
+        target: {
+          value: input,
+        },
+      } as ChangeEvent<HTMLInputElement>;
+
+      const [, , handleChange] = result.current;
+      handleChange(event);
+    });
+
+    //then
+    const [value] = result.current;
+
+    expect(value).toEqual(input);
+  });
+});
