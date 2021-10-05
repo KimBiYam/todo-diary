ded41816bc98aef3efdd1d4a17fe02714dc2172a
ChangHyunKim <hot9998@naver.com>
2021-10-05 23:25:28 +0900

    Add dialog hooks test

---
+++ webapp/src/hooks/__tests__/useDialogAction.test.ts
@@ -0,0 +1,96 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import {
+  closeDialog,
+  openDialog,
+  openDialogWithDelay,
+} from '../../reducers/dialog';
+import { RootState } from '../../reducers/rootReducer';
+import { prepareMockWrapper } from '../../utils/testUtil';
+import useDialogAction from '../useDialogAction';
+
+describe('useDialogAction hook', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  const setup = (initialState?: RootState) => {
+    const { store, wrapper } = prepareMockWrapper(initialState);
+
+    const { result } = renderHook(() => useDialogAction(), { wrapper });
+
+    return { store, result };
+  };
+
+  it('should success call open dialog action', () => {
+    //given
+    const initialState = {
+      dialog: { isOpen: false, text: undefined },
+    } as RootState;
+
+    //when
+    const { store, result } = setup(initialState);
+
+    act(() => result.current.openDialog('test'));
+
+    //then
+    const isOpenDialogWithDelayCalled = store
+      .getActions()
+      .some((action) => action.type === openDialogWithDelay.pending.type);
+
+    const isOpenDialogCalled = store
+      .getActions()
+      .some((action) => action.type === openDialog.type);
+
+    expect(isOpenDialogWithDelayCalled).toEqual(true);
+    expect(isOpenDialogCalled).toEqual(true);
+  });
+
+  it('should call close dialog fulfilled action when after opening a dialog and openDialogTime elapsed', async () => {
+    //given
+    const initialState = {
+      dialog: { isOpen: false, text: undefined },
+    } as RootState;
+
+    const openDialogTime = 1000;
+
+    //when
+    const { store, result } = setup(initialState);
+
+    act(() => {
+      result.current.openDialog('test', openDialogTime);
+    });
+
+    jest.runAllTimers();
+
+    //then
+    setTimeout(() => {
+      const isCloseDialogCalled = store
+        .getActions()
+        .some((action) => action.type === closeDialog.type);
+
+      expect(isCloseDialogCalled).toEqual(true);
+    }, openDialogTime);
+  });
+
+  it('should success call close dialog action', () => {
+    //given
+    const initialState = {
+      dialog: { isOpen: true, text: 'test' },
+    } as RootState;
+
+    //when
+    const { store, result } = setup(initialState);
+
+    act(() => {
+      result.current.closeDialog();
+    });
+
+    //then
+    expect(store.getActions()[0].type).toEqual(closeDialog.type);
+  });
+});
---
+++ webapp/src/hooks/__tests__/useDialogSelector.test.ts
@@ -0,0 +1,27 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { RootState } from '../../reducers/rootReducer';
+import { prepareMockWrapper } from '../../utils/testUtil';
+import useDialogSelector from '../useDialogSelector';
+
+describe('useDialogSelector hook', () => {
+  const setup = (initialState?: RootState) => {
+    const { wrapper, store } = prepareMockWrapper(initialState);
+
+    const { result } = renderHook(() => useDialogSelector(), { wrapper });
+
+    return { store, result };
+  };
+
+  it("should success get store's dialog state", () => {
+    //given
+    const initialState = {
+      dialog: { isOpen: false, text: undefined },
+    } as RootState;
+
+    //when
+    const { result } = setup(initialState);
+
+    //then
+    expect(result.current.dialog).toEqual(initialState.dialog);
+  });
+});
