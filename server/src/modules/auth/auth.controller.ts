82cecb70baa79015ab589474beca1217d5c61eab
ChangHyunKim <hot9998@naver.com>
2022-01-06 00:24:00 +0900

    Implement getSocialAccountDtoByGithubCode method in auth service

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -80,20 +80,10 @@ export class AuthController {
       '깃허브 유저 정보를 가져온 뒤 서버에 존재하지 않는 계정이면 생성 후 로그인합니다',
   })
   @ApiResponse({ status: 201, description: '깃허브 소셜 로그인 성공' })
-  async callbackGithubOAuth(@Body() { code }: GithubOAuthDTO) {
-    const githubToken = await this.authService.getGithubAccessToken(code);
-
-    const socialAccountDto = await this.authService.getGithubProfile(
-      githubToken,
+  async githubLogin(@Body() { code }: GithubOAuthDTO) {
+    const socialAccountDto = await this.authService.getSocialAccountDtoByGithubCode(
+      code,
     );
-
-    const isSocialAccountExists = await this.authService.isSocialAccountExists(
-      socialAccountDto,
-    );
-
-    if (!isSocialAccountExists) {
-      await this.authService.createSocialAccount(socialAccountDto);
-    }
 
     const { email } = socialAccountDto;
 
