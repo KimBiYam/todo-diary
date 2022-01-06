4635f95fa71aebb946357f0d5620a1a8f0ad14a4
ChangHyunKim <hot9998@naver.com>
2022-01-07 02:05:39 +0900

    Add deleteMyDiary test case for diary controller

--- server/src/modules/diary/diary.controller.spec.ts
+++ server/src/modules/diary/diary.controller.spec.ts
@@ -1,26 +1,63 @@
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { user } from '@src/__fixtures__/user/user';
+import { mockDiaryService } from '@src/__mocks__/services/mock.diary.service';
+import { mockUserService } from '@src/__mocks__/services/mock.user.service';
+import { DeleteResult } from 'typeorm';
 import { UserService } from '../user';
+import { RequestUserDto } from '../user/dto';
 import { DiaryController } from './diary.controller';
 import { DiaryService } from './diary.service';
 
 describe('DiaryController', () => {
-  let controller: DiaryController;
+  let diaryController: DiaryController;
+  let diaryService: DiaryService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DiaryController],
       providers: [
-        { provide: DiaryService, useValue: {} },
-        { provide: UserService, useValue: {} },
+        mockDiaryService,
+        mockUserService,
         { provide: JwtService, useValue: {} },
       ],
     }).compile();
 
-    controller = module.get<DiaryController>(DiaryController);
+    diaryController = module.get<DiaryController>(DiaryController);
+    diaryService = module.get<DiaryService>(DiaryService);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(diaryController).toBeDefined();
+  });
+
+  describe('deleteMyDiary', () => {
+    it('return msg when succeed delete my diary', async () => {
+      //given
+      const requestUserDto: RequestUserDto = {
+        id: '1',
+        displayName: 'displayName',
+      };
+
+      const diaryId = 1;
+
+      userService.findUserById = jest.fn().mockResolvedValue(user);
+
+      const deleteMyDiarySpy = jest
+        .spyOn(diaryService, 'deleteMyDiary')
+        .mockResolvedValue(new DeleteResult());
+
+      //when
+      const result = await diaryController.deleteMyDiary(
+        requestUserDto,
+        diaryId,
+      );
+
+      //then
+      expect(result.msg).toEqual('Successfully deleted your diary');
+      expect(deleteMyDiarySpy).toBeCalledWith(user, diaryId);
+    });
   });
 });
