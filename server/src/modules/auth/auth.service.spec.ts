489a4dc1137e176c832e2e4fba0249c89d748ab1
ChangHyunKim <hot9998@naver.com>
2022-01-07 00:38:11 +0900

    Fix test case mock denpendencies

--- server/src/modules/auth/auth.service.spec.ts
+++ server/src/modules/auth/auth.service.spec.ts
@@ -6,6 +6,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { SocialAccount } from '@src/entities';
 import { Connection } from 'typeorm';
 import { UserService } from '../user';
+import { UserRepository } from '../user/user.repository';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -21,6 +22,7 @@ describe('AuthService', () => {
         { provide: ConfigService, useValue: {} },
         { provide: Connection, useValue: {} },
         { provide: getRepositoryToken(SocialAccount), useValue: {} },
+        { provide: UserRepository, useValue: {} },
       ],
     }).compile();
 
