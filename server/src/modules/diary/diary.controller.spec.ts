e40c59c7ee27f214e202eb13622455ea188c173f
ChangHyunKim <hot9998@naver.com>
2022-01-08 01:35:54 +0900

    Add updateMyDiary test case for diary controller

--- server/src/modules/diary/diary.controller.spec.ts
+++ server/src/modules/diary/diary.controller.spec.ts
@@ -1,5 +1,8 @@
+import { BadRequestException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Diary } from '@src/entities';
+import { diary } from '@src/__fixtures__/diary/diary';
 import { user } from '@src/__fixtures__/user/user';
 import { mockDiaryService } from '@src/__mocks__/services/mock.diary.service';
 import { mockUserService } from '@src/__mocks__/services/mock.user.service';
@@ -8,6 +11,7 @@ import { UserService } from '../user';
 import { RequestUserDto } from '../user/dto';
 import { DiaryController } from './diary.controller';
 import { DiaryService } from './diary.service';
+import { UpdateDiaryDto } from './dto';
 
 describe('DiaryController', () => {
   let diaryController: DiaryController;
@@ -31,6 +35,75 @@ describe('DiaryController', () => {
 
   it('should be defined', () => {
     expect(diaryController).toBeDefined();
+  });
+
+  describe('updateMyDiary', () => {
+    it('return diary when succeed update my diary', async () => {
+      //given
+      const requestUserDto: RequestUserDto = {
+        id: '1',
+        displayName: 'displayName',
+      };
+
+      const diaryId = 1;
+
+      const updateDiaryDto: UpdateDiaryDto = {
+        title: 'updated title',
+        isFinished: true,
+        content: 'updated content',
+      };
+
+      const { title, isFinished, content } = updateDiaryDto;
+
+      const updatedDiary: Diary = diary;
+      updatedDiary.title = title;
+      updatedDiary.isFinished = isFinished;
+      updatedDiary.diaryMeta.content = content;
+
+      userService.findUserById = jest.fn().mockResolvedValue(user);
+      diaryService.updateMyDiary = jest.fn().mockResolvedValue(updatedDiary);
+
+      //when
+      const result = await diaryController.updateMyDiary(
+        requestUserDto,
+        updateDiaryDto,
+        diaryId,
+      );
+
+      //then
+      expect(result).toEqual({ diary });
+      expect(result.diary.title).toEqual(title);
+      expect(result.diary.isFinished).toEqual(isFinished);
+      expect(result.diary.diaryMeta.content).toEqual(content);
+    });
+
+    it('throw error when failed update my diary', async () => {
+      //given
+      const requestUserDto: RequestUserDto = {
+        id: '1',
+        displayName: 'displayName',
+      };
+
+      const updateDiaryDto: UpdateDiaryDto = {
+        title: 'updated title',
+        isFinished: true,
+        content: 'updated content',
+      };
+
+      const diaryId = 1;
+
+      userService.findUserById = jest.fn().mockResolvedValue(user);
+      diaryService.updateMyDiary = jest
+        .fn()
+        .mockRejectedValue(new BadRequestException());
+
+      //when
+
+      //then
+      await expect(
+        diaryController.updateMyDiary(requestUserDto, updateDiaryDto, diaryId),
+      ).rejects.toThrowError(BadRequestException);
+    });
   });
 
   describe('deleteMyDiary', () => {
@@ -58,6 +131,28 @@ describe('DiaryController', () => {
       //then
       expect(result.msg).toEqual('Successfully deleted your diary');
       expect(deleteMyDiarySpy).toBeCalledWith(user, diaryId);
+    });
+
+    it('throw error when failed delete my diary', async () => {
+      //given
+      const requestUserDto: RequestUserDto = {
+        id: '1',
+        displayName: 'displayName',
+      };
+
+      const diaryId = 1;
+
+      userService.findUserById = jest.fn().mockResolvedValue(user);
+      diaryService.deleteMyDiary = jest
+        .fn()
+        .mockRejectedValue(new BadRequestException());
+
+      //when
+
+      //then
+      await expect(
+        diaryController.deleteMyDiary(requestUserDto, diaryId),
+      ).rejects.toThrowError(BadRequestException);
     });
   });
 });
