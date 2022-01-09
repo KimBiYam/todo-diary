76d8bad7015c7de9a589d793b0df9452b7c139fd
ChangHyunKim <hot9998@naver.com>
2022-01-09 22:19:32 +0900

    Add diary crud api test on diary controller

--- server/src/modules/diary/diary.controller.spec.ts
+++ server/src/modules/diary/diary.controller.spec.ts
@@ -11,12 +11,14 @@ import { UserService } from '../user';
 import { RequestUserDto } from '../user/dto';
 import { DiaryController } from './diary.controller';
 import { DiaryService } from './diary.service';
-import { UpdateDiaryDto } from './dto';
+import { CreateDiaryDto, UpdateDiaryDto } from './dto';
 
 describe('DiaryController', () => {
   let diaryController: DiaryController;
   let diaryService: DiaryService;
   let userService: UserService;
+
+  let requestUserDto: RequestUserDto;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,20 +33,100 @@ describe('DiaryController', () => {
     diaryController = module.get<DiaryController>(DiaryController);
     diaryService = module.get<DiaryService>(DiaryService);
     userService = module.get<UserService>(UserService);
+
+    requestUserDto = {
+      id: '1',
+      displayName: 'displayName',
+    };
   });
 
   it('should be defined', () => {
     expect(diaryController).toBeDefined();
   });
 
+  describe('createDiary', () => {
+    it('return serialized diary when succeed create diary', async () => {
+      //given
+      const createDiaryDto: CreateDiaryDto = {
+        title: 'title',
+        content: 'content',
+      };
+
+      const diary = new Diary();
+      diary.id = '1';
+      diary.createdAt = new Date('2020-01-01');
+      diary.title = 'title';
+      diary.isFinished = false;
+      diary.diaryMeta = {
+        id: '1',
+        content: 'content',
+        diary,
+      };
+
+      userService.findUserById = jest.fn().mockResolvedValue(user);
+
+      const createDiarySpy = jest
+        .spyOn(diaryService, 'createDiary')
+        .mockResolvedValue(diary);
+
+      //when
+      const result = await diaryController.createDiary(
+        requestUserDto,
+        createDiaryDto,
+      );
+
+      //then
+      const serialized = {
+        id: '1',
+        createdAt: new Date('2020-01-01'),
+        isFinished: false,
+        title: 'title',
+        content: 'content',
+      };
+
+      expect(createDiarySpy).toBeCalledWith(requestUserDto, createDiaryDto);
+      expect(result).toEqual({ diary: serialized });
+    });
+  });
+
+  describe('findMyDiary', () => {
+    it('return serialized diary when succeed find my diary', async () => {
+      //given
+      const diaryId = 1;
+
+      const diary = new Diary();
+      diary.id = '1';
+      diary.createdAt = new Date('2020-01-01');
+      diary.title = 'title';
+      diary.isFinished = false;
+      diary.diaryMeta = {
+        id: '1',
+        content: 'content',
+        diary,
+      };
+
+      userService.findUserById = jest.fn().mockResolvedValue(user);
+      diaryService.findMyDiary = jest.fn().mockResolvedValue(diary);
+
+      //when
+      const result = await diaryController.findMyDiary(requestUserDto, diaryId);
+
+      //then
+      const serialized = {
+        id: '1',
+        createdAt: new Date('2020-01-01'),
+        isFinished: false,
+        title: 'title',
+        content: 'content',
+      };
+
+      expect(result).toEqual({ diary: serialized });
+    });
+  });
+
   describe('updateMyDiary', () => {
     it('return diary when succeed update my diary', async () => {
       //given
-      const requestUserDto: RequestUserDto = {
-        id: '1',
-        displayName: 'displayName',
-      };
-
       const diaryId = 1;
 
       const updateDiaryDto: UpdateDiaryDto = {
@@ -79,11 +161,6 @@ describe('DiaryController', () => {
 
     it('throw error when failed update my diary', async () => {
       //given
-      const requestUserDto: RequestUserDto = {
-        id: '1',
-        displayName: 'displayName',
-      };
-
       const updateDiaryDto: UpdateDiaryDto = {
         title: 'updated title',
         isFinished: true,
@@ -109,11 +186,6 @@ describe('DiaryController', () => {
   describe('deleteMyDiary', () => {
     it('return msg when succeed delete my diary', async () => {
       //given
-      const requestUserDto: RequestUserDto = {
-        id: '1',
-        displayName: 'displayName',
-      };
-
       const diaryId = 1;
 
       userService.findUserById = jest.fn().mockResolvedValue(user);
@@ -135,11 +207,6 @@ describe('DiaryController', () => {
 
     it('throw error when failed delete my diary', async () => {
       //given
-      const requestUserDto: RequestUserDto = {
-        id: '1',
-        displayName: 'displayName',
-      };
-
       const diaryId = 1;
 
       userService.findUserById = jest.fn().mockResolvedValue(user);
