120bdd89e94331d0f3e7d54d2a7c5096f93bfa08
ChangHyunKim <hot9998@naver.com>
2022-01-10 14:14:16 +0900

    Add get diary methods test cases for diary service

--- server/src/modules/diary/diary.service.spec.ts
+++ server/src/modules/diary/diary.service.spec.ts
@@ -1,16 +1,19 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Diary } from '@src/entities';
+import { diary } from '@src/__fixtures__/diary/diary';
 import { user } from '@src/__fixtures__/user/user';
 import { Connection } from 'typeorm';
 import { UserService } from '../user';
 import { DiaryRepository } from './diary.repository';
 import { DiaryService } from './diary.service';
-import { DiariesExistsDatesDto } from './dto';
+import { DiariesExistsDatesDto, GetDiariesDto } from './dto';
 import { DiariesStatisticsDto } from './dto/diaries-statistics.dto';
 
 describe('DiaryService', () => {
   let diaryService: DiaryService;
+  let diaryRepository: DiaryRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,10 +26,141 @@ describe('DiaryService', () => {
     }).compile();
 
     diaryService = module.get<DiaryService>(DiaryService);
+    diaryRepository = module.get<DiaryRepository>(DiaryRepository);
   });
 
   it('should be defined', () => {
     expect(diaryService).toBeDefined();
+  });
+
+  describe('findMyDiaries', () => {
+    it('return diaries by date when defined createdDate in getDiariesDto', async () => {
+      // given
+      const getDiariesDto: GetDiariesDto = {
+        limit: 10,
+        page: 1,
+        createdDate: '2020-01-01',
+      };
+
+      const diary1 = new Diary();
+      diary1.createdAt = new Date('2020-01-01');
+
+      const diary2 = new Diary();
+      diary2.createdAt = new Date('2020-01-01');
+
+      const diaries: Diary[] = [diary1, diary2];
+
+      diaryRepository.findMyDiariesByDate = jest
+        .fn()
+        .mockResolvedValue(diaries);
+
+      // when
+      const result = await diaryService.findMyDiaries(user, getDiariesDto);
+
+      // then
+      expect(result).toEqual(diaries);
+    });
+
+    it('return diaries when undefined createdDate in getDiariesDto', async () => {
+      // given
+      const getDiariesDto: GetDiariesDto = {
+        limit: 10,
+        page: 1,
+        createdDate: undefined,
+      };
+
+      const diary1 = new Diary();
+      diary1.createdAt = new Date('2020-01-01');
+
+      const diary2 = new Diary();
+      diary2.createdAt = new Date('2020-02-01');
+
+      const diaries: Diary[] = [diary1, diary2];
+
+      diaryRepository.findMyDiaries = jest.fn().mockResolvedValue(diaries);
+
+      // when
+      const result = await diaryService.findMyDiaries(user, getDiariesDto);
+
+      // then
+      expect(result).toEqual(diaries);
+    });
+  });
+
+  describe('findMyDiary', () => {
+    it('return diary when succeed find my diary', async () => {
+      //given
+      const diaryId = 1;
+
+      diaryRepository.findMyDiary = jest.fn().mockResolvedValue(diary);
+
+      //when
+      const result = await diaryService.findMyDiary(user, diaryId);
+
+      //then
+      expect(result).toEqual(diary);
+    });
+
+    it('throw exception when failed find my diary', async () => {
+      //given
+      const diaryId = 1;
+
+      diaryRepository.findMyDiary = jest.fn().mockResolvedValue(null);
+
+      //when
+
+      //then
+      await expect(
+        diaryService.findMyDiary(user, diaryId),
+      ).rejects.toThrowError(NotFoundException);
+    });
+  });
+
+  describe('findDiariesByYear', () => {
+    it('return diaries by year', async () => {
+      // given
+      const year = 2020;
+
+      const diary1 = new Diary();
+      diary1.createdAt = new Date('2020-01-01');
+
+      const diary2 = new Diary();
+      diary2.createdAt = new Date('2020-12-01');
+
+      const diaries: Diary[] = [diary1, diary2];
+
+      diaryRepository.find = jest.fn().mockResolvedValue(diaries);
+
+      // when
+      const result = await diaryService.findDiariesByYear(user, year);
+
+      // then
+      expect(result).toEqual(diaries);
+    });
+  });
+
+  describe('findDiariesByMonth', () => {
+    it('return diaries by month', async () => {
+      // given
+      const year = 2020;
+      const month = 1;
+
+      const diary1 = new Diary();
+      diary1.createdAt = new Date('2020-01-01');
+
+      const diary2 = new Diary();
+      diary2.createdAt = new Date('2020-01-10');
+
+      const diaries: Diary[] = [diary1, diary2];
+
+      diaryRepository.find = jest.fn().mockResolvedValue(diaries);
+
+      // when
+      const result = await diaryService.findDiariesByMonth(user, year, month);
+
+      // then
+      expect(result).toEqual(diaries);
+    });
   });
 
   describe('getDatesTheDiaryExists', () => {
