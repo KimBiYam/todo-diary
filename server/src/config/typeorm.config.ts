2591d4abc96d590380cd984ce722c81467050102
ChangHyunKim <hot9998@naver.com>
2022-01-21 18:19:36 +0900

    Add migration settings

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,16 +1,26 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SocialAccount, User, Diary, DiaryMeta } from '@src/entities';
+import { join } from 'path';
+import { ConnectionOptions } from 'typeorm';
 
-export const typeormConfig: TypeOrmModuleOptions = {
+const isProduction = process.env.NODE_ENV === 'PRODUCTION';
+
+const config: ConnectionOptions = {
   type: 'mariadb',
   host: process.env.DB_HOST,
   port: Number(process.env.MARIADB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DEFAULT_DATABASE,
-  synchronize: process.env.NODE_ENV === 'DEVELOPMENT',
+  synchronize: false,
+  migrationsRun: true,
   charset: process.env.DB_CHARSET,
-  keepConnectionAlive: true,
-  logging: process.env.NODE_ENV === 'DEVELOPMENT',
+  logging: !isProduction,
   entities: [User, SocialAccount, Diary, DiaryMeta],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  cli: {
+    entitiesDir: join(__dirname, '../entities'),
+    migrationsDir: join(__dirname, '../migrations'),
+  },
 };
+
+export = config;
---
+++ server/src/migrations/1642772404243-todo-diary.ts
@@ -0,0 +1,72 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class todoDiary1642772404243 implements MigrationInterface {
+  name = 'todoDiary1642772404243';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE TABLE `diary_meta` (`id` bigint NOT NULL AUTO_INCREMENT, `content` varchar(5000) NOT NULL, `diary_id` bigint NULL, UNIQUE INDEX `REL_df831d3fb38b389b49c67be741` (`diary_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB',
+    );
+    await queryRunner.query(
+      'CREATE TABLE `diary` (`id` bigint NOT NULL AUTO_INCREMENT, `title` varchar(100) NOT NULL, `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `is_finished` tinyint NOT NULL DEFAULT 0, `user_id` bigint NOT NULL, INDEX `IDX_ea6a2b034e04736dc5a79ef075` (`title`), INDEX `IDX_11d15a1668bfb1a1524c7b1035` (`created_at`), PRIMARY KEY (`id`)) ENGINE=InnoDB',
+    );
+    await queryRunner.query(
+      'CREATE TABLE `user` (`id` bigint NOT NULL AUTO_INCREMENT, `username` varchar(100) NULL, `email` varchar(100) NULL, `display_name` varchar(64) NOT NULL, `photo_url` varchar(500) NULL, `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `is_certified` tinyint NOT NULL DEFAULT 0, INDEX `IDX_d091f1d36f18bbece2a9eabc6e` (`created_at`), UNIQUE INDEX `IDX_78a916df40e02a9deb1c4b75ed` (`username`), UNIQUE INDEX `IDX_e12875dfb3b1d92d7d7c5377e2` (`email`), PRIMARY KEY (`id`)) ENGINE=InnoDB',
+    );
+    await queryRunner.query(
+      'CREATE TABLE `social_account` (`id` bigint NOT NULL AUTO_INCREMENT, `social_id` varchar(255) NOT NULL, `provider` varchar(20) NOT NULL, `user_id` bigint NULL, INDEX `IDX_dc89592d6165c79bbc6941be71` (`social_id`), INDEX `IDX_d5488b2d3463b86dd2f3c537c6` (`provider`), UNIQUE INDEX `REL_365d4084b1feb693468d624841` (`user_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB',
+    );
+    await queryRunner.query(
+      'ALTER TABLE `diary_meta` ADD CONSTRAINT `FK_df831d3fb38b389b49c67be7411` FOREIGN KEY (`diary_id`) REFERENCES `diary`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION',
+    );
+    await queryRunner.query(
+      'ALTER TABLE `diary` ADD CONSTRAINT `FK_330f20310184a92a90225c36cbe` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION',
+    );
+    await queryRunner.query(
+      'ALTER TABLE `social_account` ADD CONSTRAINT `FK_365d4084b1feb693468d6248411` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE',
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE `social_account` DROP FOREIGN KEY `FK_365d4084b1feb693468d6248411`',
+    );
+    await queryRunner.query(
+      'ALTER TABLE `diary` DROP FOREIGN KEY `FK_330f20310184a92a90225c36cbe`',
+    );
+    await queryRunner.query(
+      'ALTER TABLE `diary_meta` DROP FOREIGN KEY `FK_df831d3fb38b389b49c67be7411`',
+    );
+    await queryRunner.query(
+      'DROP INDEX `REL_365d4084b1feb693468d624841` ON `social_account`',
+    );
+    await queryRunner.query(
+      'DROP INDEX `IDX_d5488b2d3463b86dd2f3c537c6` ON `social_account`',
+    );
+    await queryRunner.query(
+      'DROP INDEX `IDX_dc89592d6165c79bbc6941be71` ON `social_account`',
+    );
+    await queryRunner.query('DROP TABLE `social_account`');
+    await queryRunner.query(
+      'DROP INDEX `IDX_e12875dfb3b1d92d7d7c5377e2` ON `user`',
+    );
+    await queryRunner.query(
+      'DROP INDEX `IDX_78a916df40e02a9deb1c4b75ed` ON `user`',
+    );
+    await queryRunner.query(
+      'DROP INDEX `IDX_d091f1d36f18bbece2a9eabc6e` ON `user`',
+    );
+    await queryRunner.query('DROP TABLE `user`');
+    await queryRunner.query(
+      'DROP INDEX `IDX_11d15a1668bfb1a1524c7b1035` ON `diary`',
+    );
+    await queryRunner.query(
+      'DROP INDEX `IDX_ea6a2b034e04736dc5a79ef075` ON `diary`',
+    );
+    await queryRunner.query('DROP TABLE `diary`');
+    await queryRunner.query(
+      'DROP INDEX `REL_df831d3fb38b389b49c67be741` ON `diary_meta`',
+    );
+    await queryRunner.query('DROP TABLE `diary_meta`');
+  }
+}
