27ab1a911990bda4740834efa1186429c059d734
ChangHyunKim <hot9998@naver.com>
2022-02-19 03:21:17 +0900

    Apply e2e settings to nest with mariaDB

---
+++ server/src/modules/user/__tests__/user.e2e.spec.ts
@@ -0,0 +1,54 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configModuleOption } from '@src/config';
+import { SocialAccount, User } from '@src/entities';
+import { SocialAccountRepository } from '@src/modules/auth/social-account.repository';
+import { getMariaDBTestTypeOrmModule } from '@src/test/util/getMariaDBTestTypeOrmModule';
+import { getConnection } from 'typeorm';
+import { UserModule } from '..';
+import { UserRepository } from '../user.repository';
+
+describe('User (e2e)', () => {
+  let app: INestApplication;
+  let userRepository: UserRepository;
+  let socialAccountRepository: SocialAccountRepository;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        UserModule,
+        ConfigModule.forRoot(configModuleOption),
+        getMariaDBTestTypeOrmModule(),
+        TypeOrmModule.forFeature([User, SocialAccount]),
+      ],
+      providers: [UserRepository, SocialAccountRepository],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    userRepository = moduleRef.get<UserRepository>(UserRepository);
+    socialAccountRepository = moduleRef.get<SocialAccountRepository>(
+      SocialAccountRepository,
+    );
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await userRepository.delete({});
+    await socialAccountRepository.delete({});
+  });
+
+  afterAll(async () => {
+    await getConnection().close();
+  });
+
+  describe('/api/v1/users/me (GET)', () => {
+    it('test', () => {
+      expect(userRepository).toBeDefined();
+      expect(socialAccountRepository).toBeDefined();
+    });
+  });
+});
