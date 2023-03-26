e05b4d1419305438c4b3d91cae32e904ad645480
Chang Hyun Kim <ckdvk9998@gmail.com>
2023-03-27 00:38:19 +0900

    Temp: normaliazation

--- webapp/src/hooks/query/useDiariesStatisticsQuery.ts
+++ webapp/src/hooks/query/useDiariesStatisticsQuery.ts
@@ -4,21 +4,23 @@ import {
   QueryGetDiariesStatisticsByYearArgs,
 } from '@generated/graphql';
 
+export const GET_DIARIES_STATISTICS_BY_YEAR_QUERY = gql`
+  query DiariesStatisticsByYear($year: Float!) {
+    getDiariesStatisticsByYear(year: $year) {
+      diariesStatisticsByYear {
+        finishedDiariesCount
+        month
+        totalCount
+      }
+    }
+  }
+`;
+
 export default function useDiariesStatisticsQuery(
   variables: QueryGetDiariesStatisticsByYearArgs,
 ) {
   return useQuery<DiariesStatisticsByYear[]>(
-    gql`
-      query DiariesStatisticsByYear($year: Float!) {
-        getDiariesStatisticsByYear(year: $year) {
-          diariesStatisticsByYear {
-            finishedDiariesCount
-            month
-            totalCount
-          }
-        }
-      }
-    `,
+    GET_DIARIES_STATISTICS_BY_YEAR_QUERY,
     { variables },
   );
 }
